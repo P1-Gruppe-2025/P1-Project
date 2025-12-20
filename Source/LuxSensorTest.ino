0861fd32d4acafef7f6685ca40d016754cbbc784
Nicklas Larsen <Sejenicklas2005@gmail.com>
2025-12-20 21:03:26 +0100

    Refactor LuxSensorTest for improved readability
    Refactor LuxSensorTest code for clarity and efficiency.

--- Source/LuxSensorTest.ino
+++ Source/LuxSensorTest.ino
@@ -2,112 +2,112 @@
 #include <Wire.h>
 #include <Adafruit_VEML7700.h>
 
-Zumo32U4Motors motors;
-Adafruit_VEML7700 veml = Adafruit_VEML7700();
+Zumo32U4Motors Motors;
+Adafruit_VEML7700 VEML = Adafruit_VEML7700();
 
 const int NUM_SCAN_STEPS = 12;     // 12 steps -> 30° per step
 const int ROTATE_DELAY = 250;      // kan tunes til ca. 30 grader
 const int DRIVE_DELAY = 800;       // frem delay
 const int FORWARD_SPEED = 200;
 const int TURN_SPEED = 150;
-int CORRECTION_SIZE = 2;           // adjustable drejning correction
+const int CORRECTION_SIZE = 2;           // adjustable drejning correction
+const int LUX_THRESHOLD = 1000;
+const int BAUD_RATE = 9600;
 
-float luxMax = 0;                  // den højeste lux målt
+float MaximumLux = 0;                  // den højeste lux målt
 
-void coarseScan(float luxReadings[]) {
-  for (int i = 0; i < NUM_SCAN_STEPS; i++) {
-    luxReadings[i] = veml.readLux();
+// Performs a 'Coarse' scan by rotating around and capturing the lux values each Index.
+void PerformCoarseScan(float LuxReadings[]) {
+  for (int Index = 0; Index < NUM_SCAN_STEPS; Index++) {
+    LuxReadings[Index] = VEML.readLux();
 
-    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    Motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
     delay(ROTATE_DELAY);
-    motors.setSpeeds(0, 0);
+    Motors.setSpeeds(0, 0);
     delay(50);
   }
 }
 
-int findBrightestStep(float luxReadings[]) {
-  int brightestStep = 0;
-  float localMax = 0;
-  for (int i = 0; i < NUM_SCAN_STEPS; i++) {
-    if (luxReadings[i] > localMax) {
-      localMax = luxReadings[i];
-      brightestStep = i;
-    }
+// Searches for the brightest index in a list of LuxReadings.
+int SearchForBrightestIndex(float LuxReadings[]) {
+  int BrightestIndex = 0;
+  float MaximumLocalLux = 0;
+  for (int Index = 0; Index < NUM_SCAN_STEPS; Index++) {
+    if (LuxReadings[Index] < MaximumLocalLux) continue;
+    MaximumLocalLux = LuxReadings[Index];
+    BrightestIndex = Index;
   }
-  luxMax = localMax; // opdaterer global luxMax
-  return brightestStep;
+
+  MaximumLux = MaximumLocalLux; // opdaterer global luxMax
+  return BrightestIndex;
 }
 
-void turnToStep(int step) {
-  int stepsCW = step;
-  int stepsCCW = NUM_SCAN_STEPS - step;
-  if (stepsCW <= stepsCCW) {
-    for (int i = 0; i < stepsCW; i++) {
-      motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+// Turns the Zumo32U4 motors towards the step index.
+void TurnTowardsStep(int Step) {
+  int StepsClockwise = Step;
+  int StepsCounterClockwise = NUM_SCAN_STEPS - Step;
+  if (StepsClockwise <= StepsCounterClockwise) { // I.. why? why can't you just do Steps instead of stepsCW? you aren't changing stepsCW anywhere.
+    for (int Index = 0; Index < StepsClockwise; Index++) {
+      Motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
       delay(ROTATE_DELAY);
-      motors.setSpeeds(0, 0);
-      delay(50);
+      Motors.setSpeeds(0, 0);
+      delay(50); // Artifical delay?
     }
   } else {
-    for (int i = 0; i < stepsCCW; i++) {
-      motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    for (int Index = 0; Index < StepsCounterClockwise; Index++) {
+      Motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
       delay(ROTATE_DELAY);
-      motors.setSpeeds(0, 0);
-      delay(50);
+      Motors.setSpeeds(0, 0);
+      delay(50); // I don't get it.
     }
   }
 }
 
 void setup() {
-  Serial.begin(9600);
-  veml.begin();
-  veml.setGain(VEML7700_GAIN_1_8);
-  veml.setIntegrationTime(VEML7700_IT_100MS);
+  Serial.begin(BAUD_RATE);
+  VEML.begin();
+  VEML.setGain(VEML7700_GAIN_1_8);
+  VEML.setIntegrationTime(VEML7700_IT_100MS);
 }
 
 void loop() {
-  float luxReadings[NUM_SCAN_STEPS];
-
-  //laver scanning
-  coarseScan(luxReadings);
-  int brightestStep = findBrightestStep(luxReadings);
-
-  // drejer til det lyseste step
-  turnToStep(brightestStep);
+  float LuxReadings[NUM_SCAN_STEPS];
+  PerformCoarseScan(LuxReadings);
+  int BrightestIndex = SearchForBrightestIndex(LuxReadings);
+  TurnTowardsStep(BrightestIndex);
 
   // kør mod lyset, men lav også corrections
-  while (true) {
-    float luxBefore = veml.readLux();
+  while (true) { // I have fifteen different ways of telling you this is bad, but they all crashed due to how bad this is; in a nutshell, creating a loop inside of a loop is going to make so many loops in such a short time, for example; 1 loop = 1 while true, 2 loop = 2 while true, and it's just going to continue until your PC eventually crashes.. or the board.
+    float PreviousLux = VEML.readLux();
 
 
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    Motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
     delay(DRIVE_DELAY);
-    motors.setSpeeds(0, 0);
+    Motors.setSpeeds(0, 0);
     delay(50);
 
-    float luxAfter = veml.readLux();
+    float PostLux = VEML.readLux();
 
     // tjekker om vi har mistet lyset
-    if (luxAfter <= luxBefore) {
+    if (PostLux <= PreviousLux) {
 
       //kører baglæns
-      motors.setSpeeds(-FORWARD_SPEED, -FORWARD_SPEED);
+      Motors.setSpeeds(-FORWARD_SPEED, -FORWARD_SPEED);
       delay(DRIVE_DELAY);
-      motors.setSpeeds(0, 0);
+      Motors.setSpeeds(0, 0);
       delay(50);
 
       // scanner igen for at finde det lyseste sted
-      coarseScan(luxReadings);
-      brightestStep = findBrightestStep(luxReadings);
-      Serial.print("New brightest step: "); Serial.println(brightestStep);
+      PerformCoarseScan(LuxReadings);
+      BrightestIndex = SearchForBrightestIndex(LuxReadings);
+      Serial.print("New brightest step: "); Serial.println(BrightestIndex);
 
-      turnToStep(brightestStep);
+      TurnTowardsStep(BrightestIndex);
       continue; // bliv ved med at køre
     }
 
-    // stopper hvis du er tæt nok på lyset
-    if (luxAfter > 1000) {  // adjust threshold for your flashlight
-      motors.setSpeeds(0, 0);
+    if (PostLux > LUX_THRESHOLD) {
+      Motors.setSpeeds(0, 0);
       Serial.println("Reached target!");
       while(1);
     }
