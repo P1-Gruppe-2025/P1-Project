8e6220babbbe003be24e305bf345eeff27439aaf
Nicklas Larsen <Sejenicklas2005@gmail.com>
2025-12-20 20:42:43 +0100

    Refactor line sensor code for clarity and consistency
    Refactor line sensor handling and improve readability with constants.

--- Source/LineSensorTest.ino
+++ Source/LineSensorTest.ino
@@ -1,55 +1,56 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 
-Zumo32U4Motors motors;
-Zumo32U4LineSensors lineSensors;
-Zumo32U4Encoders encoders;
-Zumo32U4OLED oled;
-Zumo32U4Buzzer buzzer;
+Zumo32U4Motors Motors;
+Zumo32U4LineSensors LineSensors;
+Zumo32U4OLED OLED;
 
-uint16_t lineSensorValue[3]; 
+uint16_t LineSensorLuminances[3]; // Normally, you'd do a constant for the amount of line sensors, but since the Zumo32U4 only ever has 3 of them in the front, we don't really need to make a new variable just for that one use.
 
-int threshold = 1500;
-int thresholdmid = 600;
+const int DELAY_IN_MILLISECONDS = 100; // If you're declaring a form of measurement, like meters, seconds etc, you should always do; "XXXX_IN_MEASUREMENT" for clarity, as type annotation won't work to explain that.
+const int MOVE_SPEED = 80;
+const int BAUD_RATE = 9600; // Normally, I would use 115200, as its the superior option to 9600 with literally no benefit.
+const int BLACK_THRESHOLD = 1500;
+const int GRAY_THRESHOLD = 600;
+// I wanted to switch over to Enums, but this is just a standard refactor to adhere to coding guidelines.
+const int CLEARED = 0;
+const int LEFT = 1;
+const int CENTER = 2;
+const int RIGHT = 3;
 
-bool cleared = 0;
-bool left = 1;
-int center = 2;
-int right = 3;
-
-
-void setup(){
-lineSensors.initThreeSensors();
-Serial.begin(9600);
-motors.setSpeeds(80,80);
+void setup() {
+  LineSensors.initThreeSensors();
+  Serial.begin(BAUD_RATE);
+  Motors.setSpeeds(MOVE_SPEED, MOVE_SPEED);
 }
 
-void loop(){
-oled.clear();
-lineSensors.read(lineSensorValue);
+void loop() {
+  OLED.clear();
+  LineSensors.read(LineSensorLuminances);
 
-if (lineSensorValue[0]>threshold){
-  oled.print("Venstre");
-  Serial.print("Venstre har ramt: ");
-  Serial.print(left);
-}
+  // Using Enums, we could've changed the 0 to a LEFT, making it more readable, but as said; I won't as this is a standard refactor.
+  if (LineSensorLuminances[0] > BLACK_THRESHOLD) {
+    OLED.print("Left");
+    Serial.print("Left has hit: ");
+    Serial.print(LEFT);
+  }
 
-else if (lineSensorValue[1]>thresholdmid){
-  oled.print("Center");
-  Serial.print("Center har ramt: ");
-  Serial.print(center);
-}
+  else if (LineSensorLuminances[1] > GRAY_THRESHOLD) {
+    OLED.print("Center");
+    Serial.print("Center has hit: ");
+    Serial.print(CENTER);
+  }
 
-else if (lineSensorValue[2]>threshold){
-  oled.print("Højre");
-  Serial.print("Højre har ramt: ");
-  Serial.print(right);
-}
-  
-else{
-  oled.print("Clear");
-  Serial.print("Alt er clear: ");
-  Serial.print(cleared);
-}
-delay(100);
+  else if (LineSensorLuminances[2] > BLACK_THRESHOLD) {
+    OLED.print("Right");
+    Serial.print("Right has hit: ");
+    Serial.print(RIGHT);
+  }
+
+  else {
+    OLED.print("Clear");
+    Serial.print("Is all sensors clear: ");
+    Serial.print(CLEARED);
+  }
+  delay(DELAY_IN_MILLISECONDS);
 }
