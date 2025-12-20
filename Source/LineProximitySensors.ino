e8e12e7bbafca761af2fba12888ddfe37e6d7ed8
Nicklas Larsen <Sejenicklas2005@gmail.com>
2025-12-20 20:54:08 +0100

    Refactor sensor reading functions and constants
    Refactor proximity and line sensor functions for clarity and consistency.

--- Source/LineProximitySensors.ino
+++ Source/LineProximitySensors.ino
@@ -1,131 +1,113 @@
- #include <Zumo32U4.h>
+#include <Zumo32U4.h>
 
-Zumo32U4OLED oled;
-Zumo32U4ProximitySensors prox;
-Zumo32U4Motors motors;
-Zumo32U4LineSensors lineSensors;
+Zumo32U4OLED OLED;
+Zumo32U4ProximitySensors ProximitySensors;
+Zumo32U4Motors Motors;
+Zumo32U4LineSensors LineSensors;
 
 //erklærer mængden af linesensors
-uint16_t lineSensorValue[3]; 
+uint16_t LineSensorLuminances[3];
 
 //til linesensor
-int threshold = 1500;
-int thresholdmid = 600;
+const int BACK_TIME_IN_MILLISECONDS = 375;
+const int MOVE_SPEED = 80;  // Adjusted from 100 to be consistent with the other code files.
+const int BAUD_RATE = 115200;
+const int BLACK_THRESHOLD = 1500;
+const int GRAY_THRESHOLD = 600;
+const int MINIMUM_DISTANCE_THRESHOLD_IN_CENTIMETERS = 5;
 
 //til prox sensor
-int left;
-int centerLeft;
-int centerRight;
-int right;
+int LeftCounts;
+int CenterLeftCounts;
+int CenterRightCounts;
+int RightCounts;
 
-
-
-//--------------------------------Funktioner---------------------------------
-
-
-//prox
-void readProx(){ //læs proximity sensor og gemmer værdierne i variablerne
-  prox.read();
-  left = prox.countsLeftWithLeftLeds();
-  centerLeft = prox.countsFrontWithLeftLeds();
-  centerRight = prox.countsFrontWithRightLeds();
-  right = prox.countsRightWithRightLeds();
+// Assigns the counts variables to the ones measured by the ProximitySensors.
+void ReadProximityCounts() {
+  ProximitySensors.read();
+  LeftCounts = ProximitySensors.countsLeftWithLeftLeds();
+  CenterLeftCounts = ProximitySensors.countsFrontWithLeftLeds();
+  CenterRightCounts = ProximitySensors.countsFrontWithRightLeds();
+  RightCounts = ProximitySensors.countsRightWithRightLeds();
 }
 
-//prox
-void checkForObstacleInFront(){
-  Serial.println("Checking for centerLeft/Right");
-  if (centerLeft>5 || centerRight>5){
-    Serial.print("Der er noget foran mig");
-    oled.gotoXY(0,0);
-    oled.print("Obstacle");
-    //Her skal vi med UL vente indtil man er 'x' cm fra target og derefter stoppe. Lige nu bruger vi bare delay.
-    delay(200);
-    stopZumo();
-    stopProgram();
-  }
+// Checks for obstacles using the proximity sensors.
+void CheckForObstacles() {
+  Serial.println("Checking for Left and Right obstacles.");
+  if (CenterLeftCounts <= MINIMUM_DISTANCE_THRESHOLD_IN_CENTIMETERS && CenterRightCounts <= MINIMUM_DISTANCE_THRESHOLD_IN_CENTIMETERS) return;
+
+  Serial.print("Detected obstacle.");
+  OLED.gotoXY(0, 0);
+  OLED.print("Obstacle");
+  delay(200); // Artifical delay.. AGAIN? WHY?
+  Motors.setSpeeds(0, 0);
+  PermanentlyStopProgram();
 }
 
-
-
-//line
-void readLines(){
-  lineSensors.read(lineSensorValue);
-}
-
-
-//line
-void checkForLines(){
+// I would refactor this in a heartbeat, but since this is just a standard ol' guideline checkup, this will stay.
+void CheckForLines() {
   //venstre
-  if (lineSensorValue[0]>threshold){
-    stopZumo();
+  if (LineSensorLuminances[0] > BLACK_THRESHOLD) {
+  Motors.setSpeeds(0, 0);
     Serial.print("Venstre har ramt");
-    oled.clear();
-    oled.gotoXY(0,0);
-    oled.print("Venstre");
-    oled.display();
+    OLED.clear();
+    OLED.gotoXY(0, 0);
+    OLED.print("Venstre");
+    OLED.display();
     delay(500);
-    backAway();
+    BackComfortableDistanceAwayFromObstacle();
   }
   //center
-  else if (lineSensorValue[1]>thresholdmid){
-    stopZumo();
+  else if (LineSensorLuminances[1] > GRAY_THRESHOLD) {
+  Motors.setSpeeds(0, 0);
     Serial.print("Center har ramt");
-    oled.clear();
-    oled.gotoXY(0,0);
-    oled.print("Center");
-    oled.display();
+    OLED.clear();
+    OLED.gotoXY(0, 0);
+    OLED.print("Center");
+    OLED.display();
     delay(500);
-    backAway();
+    BackComfortableDistanceAwayFromObstacle();
   }
   //højre
-  else if (lineSensorValue[2]>threshold){
-    stopZumo();
+  else if (LineSensorLuminances[2] > BLACK_THRESHOLD) {
+  Motors.setSpeeds(0, 0);
     Serial.print("Højre har ramt");
-    oled.clear();
-    oled.gotoXY(0,0);
-    oled.print("Højre");
-    oled.display();
+    OLED.clear();
+    OLED.gotoXY(0, 0);
+    OLED.print("Højre");
+    OLED.display();
     delay(500);
-    backAway();
+    BackComfortableDistanceAwayFromObstacle();
   }
 }
 
-
-//movement
-void stopZumo(){
-  motors.setSpeeds(0,0);
+// Moves backwards for BACK_TIME_IN_MILLISECONDS amount of time, then stops and waits 500ms (why?)
+void BackComfortableDistanceAwayFromObstacle() {
+  Motors.setSpeeds(-MOVE_SPEED, -MOVE_SPEED);
+  delay(BACK_TIME_IN_MILLISECONDS);
+  Motors.setSpeeds(0, 0);  // Also deemed the StopZumo function useless, as it's a wrapper for another function, making it useless.
+  delay(500);              // Artifical delay? I need an explanation.
 }
 
-//movement
-void backAway(){
-  motors.setSpeeds(-150,-150);
-  delay(200);
-  stopZumo();
-  delay(500);
+// This is a really strange way of stopping the program, but if it works it works.
+void PermanentlyStopProgram() {
+  while (true)
+    ;
 }
 
-
-//program
-void stopProgram(){
-  while(1==1);
+// Sets up the Proximity- and Linesensors, as well as the serial monitors and motors.
+void setup() {
+  ProximitySensors.initThreeSensors();
+  LineSensors.initThreeSensors();
+  Serial.begin(BAUD_RATE);
+  Motors.setSpeeds(MOVE_SPEED, MOVE_SPEED);
 }
 
-
-
-void setup(){
-  prox.initThreeSensors();
-  lineSensors.initThreeSensors();
-  Serial.begin(115200);
-  motors.setSpeeds(100,100);
-}
-
-
-void loop(){
-  readProx();
-  checkForObstacleInFront();
-  delay(200);
-  readLines();
-  checkForLines();
-  delay(200);
+// Gets called every 200ms(Why?)
+void loop() {
+  ReadProximityCounts();
+  CheckForObstacles();
+  LineSensors.read(LineSensorLuminances);  // Deemed the readLines function useless, as it's a 'facade' for just calling another function, making it useless.
+  CheckForLines();
+  delay(200);  // Also artifical? why are these delays here without explanation?
 }
